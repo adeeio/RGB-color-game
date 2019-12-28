c40807d27cca3724b01c2aa9b2ee38a4d7ddda64
Adrian Ostapkiewicz <ostapkiewicz.adrian@gmail.com>
2019-12-28 12:47:35 +0000

    cleaned up colorGame.js code

--- colorGame.js
+++ colorGame.js
@@ -1,91 +1,109 @@
 'use-strict';
 
-var randomColorRGB = document.getElementById("randomRGB");
-var squares = document.querySelectorAll(".square");
-var rgb = randomRGB();
-var randomNum = randomNumber();
+const numberOfSquares = 6;
+const arrOfRgb = [];
+const randomColorRGB = document.getElementById("randomRGB");
+const squares = document.querySelectorAll(".square");
 const jumbotron = document.getElementById("jumbotron");
 const tryAgain = document.getElementById('tryAgain');
 const newColor = document.getElementById('newColor');
 const easy = document.getElementById('easy');
 const hard = document.getElementById('hard');
-randomColorRGB.innerHTML = rgb;
 
+const randomNumber = () => { return Math.floor(Math.random() * numberOfSquares); }
+const randomNum = randomNumber();
 
-function randomNumber() {
-    return Math.floor(Math.random() * 6);
-}
+const randomRGB = () => {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
 
-function randomRGB() {
-    var r = Math.floor(Math.random() * 256);
-    var g = Math.floor(Math.random() * 256);
-    var b = Math.floor(Math.random() * 256);
-
-    var rgb = "rgb(" + r + ", " + g + ", " + b + ")";
-
+    let rgb = "rgb(" + r + ", " + g + ", " + b + ")";
     return rgb;
 }
 
-for (i = 0; i < squares.length; i++) {
-    squares[i].style.backgroundColor = randomRGB();
+const getColors = (numberOfSquares, arr) => {
+    for (let i = 0; i < numberOfSquares; i++) {
+        arr[i] = randomRGB();
+    }
+    return arr;
+}
 
-    if (i === randomNum) {
-        squares[i].style.backgroundColor = rgb;
+const colorSquares = () => {
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = arrOfRgb[i];
     }
 }
 
-
-squares.forEach(el => {
-    el.addEventListener("click", () => {
-        if (el.style.backgroundColor === rgb) {
-            changeColorOfBoxes(rgb);
+const correctColor = () => {
+    for (let i = 0; i < arrOfRgb.length; i++) {
+        if (i === randomNum) {
+            randomColorRGB.innerText = arrOfRgb[i];
         }
-        else {
-            el.style.visibility = 'hidden';
-            tryAgain.innerHTML = "Try Again!";
-        }
-    });
-});
-
-
-const changeColorOfBoxes = e => {
-    squares.forEach(el => {
-        el.style.visibility = 'visible';
-        el.style.backgroundColor = e;
-        jumbotron.style.backgroundColor = e;
-        tryAgain.innerHTML = "Correct!";
-        newColor.innerHTML = "Play Again?"
-    });
+    }
 }
 
-const easyMode = () => {
-    easy.classList.add("selected");
-    hard.classList.remove('selected');
+const clickedColor = () => {
+    squares.forEach(el => {
+        el.addEventListener("click", () => {
+            if (el.style.backgroundColor === randomColorRGB.innerText) {
+                for (let i = 0; i < squares.length; i++) {
+                    squares[i].style.backgroundColor = randomColorRGB.innerText;
+                    jumbotron.style.backgroundColor = randomColorRGB.innerText;
+                }
+
+            }
+            else {
+                el.style.backgroundColor = "black";
+            }
+        });
+    })
+}
+
+newColor.addEventListener('click', () => {
+    jumbotron.style.background = "blue";
+    getColors(numberOfSquares, arrOfRgb);
+    colorSquares();
+    correctColor();
+    clickedColor();
+});
+
+const hideBoxes = () => {
     let counter = 0;
     for (let i = 0; i < squares.length; i++) {
-        if (squares[i].style.backgroundColor !== rgb) {
-            squares[i].style.display = 'none';
+        if(squares[i].style.backgroundColor !== randomColorRGB.innerText){
+            squares[i].style.display = "none";
             counter++;
         }
-
-        if (counter === 3) {
+        if(counter === 3){
             break;
         }
     }
 }
-const hardMode = () => {
-    hard.classList.add("selected");
-    easy.classList.remove('selected');
-    for (let i = 0; i < squares.length; i++) {
-        squares[i].style.display = 'inline-block';
+
+const showBoxes = () => {
+    for(let i = 0; i < squares.length; i++){
+        squares[i].style.display = "block";
     }
 }
 
-newColor.addEventListener('click', () => document.location.reload(true));
+
+easy.addEventListener("click", () => {
+    easy.classList.add("selected");
+    hard.classList.remove("selected");
+    hideBoxes();
+});
+
+hard.addEventListener("click", () => {
+    hard.classList.add("selected");
+    easy.classList.remove("selected");
+    showBoxes();
+});
 
 
+getColors(numberOfSquares, arrOfRgb);
+colorSquares();
+correctColor();
+clickedColor();
 
-
-easy.addEventListener('click', easyMode);
-hard.addEventListener('click', hardMode);
 
